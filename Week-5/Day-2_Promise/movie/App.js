51a7550bdf05dcb04234edef391112099b8bccfd
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-03-16 17:53:26 +0700

    Tugas Day 2 Week-5

--- Week-5/Day-2_Promise/movie/App.js
+++ Week-5/Day-2_Promise/movie/App.js
@@ -1,40 +1,89 @@
 import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { ACCESS_TOKEN, BASE_URL } from './src/helpers/apiAccessToken'
+import { ACCESS_TOKEN, BASE_URL, ImageUrl } from './src/helpers/apiAccessToken'
 
 const App = () => {
   const [listMovie, setListMovie] = useState([])
+  const [title, setTitle] = useState("List Movie")
 
-  const getListMovie = async () => {
+  useEffect(() => {
+    getPopularMovie()
+  }, [])
+
+  const getPopularMovie = async () => {
     try {
       const results = await axios.get(`${BASE_URL}/movie/popular`, { headers: { Authorization: `Bearer ${ACCESS_TOKEN}` } })
-
+      console.log(results);
       setListMovie(results.data.results)
+      setTitle("List Popular Movie")
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(() => {
-    getListMovie()
-  }, [])
+  const getTopRatedMovie = async () => {
+    try {
+      const results = await axios.get(`${BASE_URL}/movie/top_rated`, { headers: { Authorization: `Bearer ${ACCESS_TOKEN}` } })
+      console.log(results);
+      setListMovie(results.data.results)
+      setTitle("List Top Rated Movie")
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  const cardMovie = ((item) => {
+  const getNowPlayingMovie = async () => {
+    try {
+      const results = await axios.get(`${BASE_URL}/movie/now_playing`, { headers: { Authorization: `Bearer ${ACCESS_TOKEN}` } })
+      console.log(results);
+      setListMovie(results.data.results)
+      setTitle("List Now Playing Movie")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const CardMovie = (({ item }) => {
     return (
-      <View>
-        <Image />
-        <Text>
-          {item.title}
-        </Text>
+      <View style={{ marginBottom: 10, flexDirection: 'row', alignItems: 'center', borderColor: '#00000026', borderWidth: 1, padding: 10, borderRadius: 5 }}>
+        <Image
+          source={{ uri: `${ImageUrl}${item.poster_path}` }}
+          style={{ height: 250, width: 150, borderRadius: 5, flex: 1, }}
+        />
+        <View style={{ marginStart: 10, flex: 1, }}>
+          <Text style={{ fontSize: 18, alignSelf: 'center', textAlign: 'center' }}>
+            {item.title}
+          </Text>
+          <Text style={{ fontSize: 14, marginTop: 10, textAlign: 'justify' }}>
+            {item.overview}
+          </Text>
+        </View>
       </View>
     )
   })
 
   return (
-    <View>
-      <Text>list Movie</Text>
-      <FlatList data={listMovie} keyExtractor={(item, index) => index} renderItem={cardMovie} />
+    <View style={{ flex: 1, marginHorizontal: 10, }}>
+      <View style={{ marginTop: 10 }}>
+        <Text style={{ fontSize: 35, color: '#630606', alignSelf: 'center', }}> MOVIE </Text>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', backgroundColor: '#F3E9DD50', marginTop: 10 }}>
+        <TouchableOpacity style={{ padding: 10, }} onPress={getPopularMovie}>
+          <Text>Popular</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ padding: 10, }} onPress={getTopRatedMovie}>
+          <Text>Top Rated</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ padding: 10, }} onPress={getNowPlayingMovie}>
+          <Text>Now Playing</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ fontSize: 20, paddingTop: 10, color: '#BB6464', marginBottom: 10 }}>{title}</Text>
+      <FlatList data={listMovie} keyExtractor={(item, index) => index} renderItem={CardMovie} />
     </View>
 
   )
--- Week-5/Day-2_Promise/movie/src/helpers/apiAccessToken.js
+++ Week-5/Day-2_Promise/movie/src/helpers/apiAccessToken.js
@@ -1,3 +1,5 @@
 export const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYmM2MmRiNjNkNTFiNDFmNzdiMmEwYTFkNWFlNGIyYyIsInN1YiI6IjYwOTI0MzM3NTQ1MDhkMDA2ZjFlMmU4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y-dWnWlInitMZLgz92o1TKqqXbnUEuYj2K3oAx9kcDg"
 
 export const BASE_URL = "https://api.themoviedb.org/3"
+
+export const ImageUrl = 'https://image.tmdb.org/t/p/original'
