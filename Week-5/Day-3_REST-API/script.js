f555ea0a8cdf83edc94cf0c84623bfdf0e2634d3
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-03-16 22:19:41 +0700

    Week-5 Day 3 RestAPI

---
+++ Week-5/Day-3_REST-API/script.js
@@ -0,0 +1,36 @@
+console.log("Day 3");
+
+//API
+//sumber informasi/data yang digunakan untuk suatu aplikasi
+// -> bagaimana menyelesaikan suatu masalah 
+
+//Rest API
+//penghubung dg sumber
+//cara berhubungan dengan API
+
+//httprequest
+// - Axios
+// - Fetch
+
+//https (secure) -> request (untuk meminta/jalur) 
+//-> status (rambu2) 404(not found, salah method/url) 200(ok/success) 201(created) 400(bad request, tidak sesuai parameter/bntuk data)
+//get ambil data
+//post ngirim data
+//put/patch update data
+//delete hapus data
+
+//301
+//redirect (permanen gabisa diganti)
+
+//302
+//redirect tempoorari bisa balik lagike aplikasi sebelumnya
+
+//307
+//redirect temporary pkdata
+
+//401
+//unauthorizzed(tak terdaftar) -> headers authorization bearer dfgdgffd
+
+
+//404
+// >500 (bad gateway, error server)
