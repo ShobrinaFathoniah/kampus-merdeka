75fc0141ac75648eb875d311e9f91c4719a95cd4
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-04-25 22:55:15 +0700

    feat: crashlytics + analytics (masih error)

--- Week-11/percobaan/App.js
+++ Week-11/percobaan/App.js
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableOpacity, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import crashlytics from '@react-native-firebase/crashlytics';
+import analytics from '@react-native-firebase/analytics';
 
 const App = () => {
+  useEffect(() => {
+    crashlytics().log('App mounted.');
+  });
+
   return (
     <View>
       <Text>App</Text>
+      <TouchableOpacity onPress={() => crashlytics().crash()}>
+        <Text>Test</Text>
+      </TouchableOpacity>
+      <View>
+        <Button
+          title="Add To Basket"
+          onPress={async () =>
+            await analytics().logEvent('login', {
+              id: 3745092,
+              item: 'mens grey t-shirt',
+              description: ['round neck', 'long sleeved'],
+              size: 'L',
+            })
+          }
+        />
+      </View>
     </View>
   );
 };
