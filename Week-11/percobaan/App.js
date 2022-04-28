43e9567179b56f35e8b8bbf19f6d87988362f4da
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-04-28 23:07:26 +0700

    feat/fix: geloc + nootif + crash

--- Week-11/percobaan/App.js
+++ Week-11/percobaan/App.js
@@ -2,10 +2,16 @@ import {View, Text, TouchableOpacity, Button} from 'react-native';
 import React, {useEffect} from 'react';
 import crashlytics from '@react-native-firebase/crashlytics';
 import analytics from '@react-native-firebase/analytics';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
   useEffect(() => {
     crashlytics().log('App mounted.');
+    // const unsubscribe = messaging().onMessage(async remoteMessage => {
+    //   Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    // });
+
+    // return unsubscribe;
   });
 
   return (
@@ -17,14 +23,16 @@ const App = () => {
       <View>
         <Button
           title="Add To Basket"
-          onPress={async () =>
+          onPress={async () => {
             await analytics().logEvent('login', {
               id: 3745092,
               item: 'mens grey t-shirt',
               description: ['round neck', 'long sleeved'],
               size: 'L',
-            })
-          }
+            });
+            const token = await messaging().getToken();
+            console.log(token, 'tooken');
+          }}
         />
       </View>
     </View>
